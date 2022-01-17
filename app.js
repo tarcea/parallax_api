0f0c6bddf78959d50644a763b906228fcd797747
tarcea <tarcea@gmail.com>
2022-01-17 14:33:25 +0100

    set the response at json

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { bgs } from "./data.js";
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(cors());
+// app.use(express.json());
 
 app.get('/', (req, res) => {
   const rndInt = Math.floor(Math.random() * bgs.length) + 1;
